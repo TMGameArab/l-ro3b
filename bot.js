488ccd5dd4c6f231e55be9b43f0baa01deb481bc
AMr404 <36173199+AMr404@users.noreply.github.com>
2018-06-29 14:23:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = '&';
-const ms = require("ms");
 const fs = require("fs");
 var Canvas = require('canvas');
 var jimp = require('jimp');
